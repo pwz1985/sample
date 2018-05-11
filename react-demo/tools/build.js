14c8b06f95ca7f6ea2b23a35b038d34fa5c11e5f
peng <>
2018-05-11 14:29:06 +0800

    fix

--- react-demo/tools/build.js
+++
@@ -1,8 +0,0 @@
-// var config = requi'../webpack.config';
-var webpack = require('webpack');
-var config = require('../webpack.config')
-
-new webpack(config, function() {
-  console.log('编译完成');
-  console.log(arguments);
-});
